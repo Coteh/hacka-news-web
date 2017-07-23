29428ed078851688744e5c35418e7f10f9b540db
Coteh <james_c_34@live.com>
2017-07-23 19:41:08 -0400

    Store current theme as cookie

--- src/header.js
+++ src/header.js
@@ -1,13 +1,26 @@
+function changeTheme(theme) {
+    switch (theme) {
+        case "regular":
+            $('.ui.segment.darkorange').addClass("orange").removeClass("darkorange inverted");
+            $('.ui.menu').removeClass("inverted");
+            $(document.body).css("background-color", "white").css("color", "black");
+            break;
+        case "night":
+            $('.ui.segment.orange').addClass("darkorange inverted").removeClass("orange");
+            $('.ui.menu').addClass("inverted");
+            $(document.body).css("background-color", "black").css("color", "white");
+            break;
+    }
+    $.cookie("theme", theme);
+}
+
 $(document).ready(function() {
     $('.ui.dropdown').dropdown();
     $('.ui.dropdown .menu .item#regular').on('click', function() {
-        $('.ui.segment.darkorange').addClass("orange").removeClass("darkorange inverted");
-        $('.ui.menu').removeClass("inverted");
-        $(document.body).css("background-color", "white").css("color", "black");
+        changeTheme("regular");
     });
     $('.ui.dropdown .menu .item#night').on('click', function() {
-        $('.ui.segment.orange').addClass("darkorange inverted").removeClass("orange");
-        $('.ui.menu').addClass("inverted");
-        $(document.body).css("background-color", "black").css("color", "white");
+        changeTheme("night");
     });
+    changeTheme($.cookie("theme"));
 });
