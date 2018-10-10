632f7ae4da1c9b35849f2dbeed5d3e6721d9af14
Coteh <james_c_34@live.com>
2018-10-09 23:11:03 -0400

    Add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 /*******************************
-    Set-up for Semantic UI
+        General Set-up
 *******************************/
 
-const SEMANTIC_TASKS_PATH = "./node_modules/semantic-ui/tasks";
+const PROJECT_ROOT = '.';
+const SITE_ROOT = 'public';
+const TEMPLATES_ROOT = 'views';
 
 var
     gulp = require('gulp-help')(require('gulp')),
+    browserSync = require('browser-sync').create()
+    ;
 
+
+/*******************************
+    Set-up for Semantic UI
+*******************************/
+
+const SEMANTIC_TASKS_PATH = './node_modules/semantic-ui/tasks';
+
+var
     // watch changes
     watch = require(SEMANTIC_TASKS_PATH + '/watch'),
 
@@ -29,11 +41,23 @@ gulp.task('default', false, [
     'watch'
 ]);
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        files: [SITE_ROOT + '/**', TEMPLATES_ROOT + '/**'],
+        // server: {
+        //     baseDir: PROJECT_ROOT + '/'
+        // },
+        proxy: 'localhost:3000'
+    });
+});
+
 gulp.task('watch', 'Watch for site/theme changes', watch);
 
 gulp.task('build', 'Builds all files from source', build);
 gulp.task('build-javascript', 'Builds all javascript from source', buildJS);
 gulp.task('build-css', 'Builds all css from source', buildCSS);
 gulp.task('build-assets', 'Copies all assets from source', buildAssets);
+
+gulp.task('dev', 'Builds site and runs in dev mode', ['build', 'browser-sync']);
 
 gulp.task('clean', 'Clean dist folder', clean);
